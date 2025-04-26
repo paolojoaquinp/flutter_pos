e80f01f27f4946f7d1537b6bc239ff4b50d53879
Paolo Joaquin Pinto <paolojoaquinpintoperez@gmail.com>
2025-04-26 16:16:23 -0400

    chore: Update API base URL configuration for deployment
    - Changed the base URL in ApiConfig from a specific IP address to a placeholder for better flexibility during deployment.
    - This update allows developers to easily replace the placeholder with the actual API URL as needed.

--- lib/core/config/api_config.dart
+++ lib/core/config/api_config.dart
@@ -1,5 +1,5 @@
 class ApiConfig {
   /// Base URL for the API
   /// Replace this with the actual API URL when deploying
-  static const String baseUrl = 'http://192.168.1.13:3000';
+  static const String baseUrl = 'http://<localhost or you IP>:3000';
 } 
